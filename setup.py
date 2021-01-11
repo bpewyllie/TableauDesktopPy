39bb69efa0e4c2b364869d46c4b1771b54284bf9
bwyllie <bwyllie@campbellcompanies.com>
2021-01-11 15:12:21 -0700

    upload to pypi

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="TableauDesktopPy-bpewyllie", # Replace with your own username
-    version="0.0.1",
+    name="TableauDesktopPy",
+    version="0.0.5",
     author="Brady Wyllie",
     author_email="bpewyllie@gmail.com",
     description="Tools for extracting metadata from Tableau Desktop workbook files.",
